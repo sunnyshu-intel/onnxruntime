dcb8e6a1589fbd3d051ea6af86a17d11920699f8
Guangyun Han <guangyunhan@microsoft.com>
2022-04-12 14:33:36 +0800

    Impl with memory allocation

--- onnxruntime/core/platform/android/cxa_demangle.cc
+++ onnxruntime/core/platform/android/cxa_demangle.cc
@@ -1,8 +1,8 @@
 // #if USE_DUMMY_EXA_DEMANGLE
 
 extern "C" {
+#include <stdlib.h>
 #include <string.h>
-#include <stddef.h>
 #include <limits.h>
 
 #define DEMANGLE_SUCCESS 0
@@ -16,29 +16,43 @@ extern "C" {
  * it contribute a large percentage of binary size in minial build. To avoid it, the LLVM user can compile libc++abi.a
  * with LIBCXXABI_NON_DEMANGLING_TERMINATE defined, see https://reviews.llvm.org/D88189 But this make the compilation
  * process extremely complex. Here we provide a dummy __cxa_demangle. It always copy the mangled name to output buffer.
- * The required allocation behavior in Itanium C++ ABI will result a failure status, a.k.a, not implemented.
  */
-char* __cxa_demangle(const char* mangled_name, char* output_buffer, size_t* length, int* status) {
+char* __cxa_demangle(const char* mangled_name, char* buf, size_t* n, int* status) {
   if (!status) return NULL;
-  if (!output_buffer) {
-    // In the API doc, the output buffer can be null. But we are allocation free, so we just return a memory allocation
-    // failure to indicate it is not the caller's fault.
-    *status = DEMANGLE_MEMORY_ALLOCATION_FAILURE;
-    return NULL;
-  }
-  if (!mangled_name || !length || (*length == 0) || (*length > INT_MAX)) {
+  if (!mangled_name ||                             // Input must be valid
+      (buf && !n) ||                               // If it is non-null, then n must also be nonnull
+      (buf && n && ((*n == 0) || (*n > INT_MAX)))  // if buf and n is supplied, *n must be a 'valid' value
+  ) {
     *status = DEMANGLE_INVALID_ARGUMENTS;
     return NULL;
   }
 
+  // size is the buffer size, including the null char
+  size_t output_size = n ? *n : 0;
   // It is caller's responsibility to ensure mangled_name is null-terminated as it is required in the API doc.
-  strncpy(output_buffer, mangled_name, *length);
+  size_t input_size = strlen(mangled_name) + 1;
+  if (input_size > output_size) {
+    free(buf);
+    buf = NULL;
+  }
+  if (buf == NULL) {
+    output_size = input_size;
+    buf = static_cast<char*>(malloc(output_size));
+    if (buf == NULL) {
+      *status = DEMANGLE_MEMORY_ALLOCATION_FAILURE;
+      return NULL;
+    }
+  }
+  strncpy(buf, mangled_name, output_size);
   // Blindly guard the output buffer, this might cause a truncated mangled name being returned without error status,
   // but this should be fine for debugging purpose. We are return the mangle name directly, if the caller is doing
   // crazy stuff with the name, it should both fail with the return mangled name and a truncated mangled name.
-  output_buffer[*length - 1] = '\0';
+  buf[output_size - 1] = '\0';
+  if (n) {
+    *n = output_size;
+  }
   *status = DEMANGLE_SUCCESS;
-  return output_buffer;
+  return buf;
 }
 }
 
